7467e5530dd3ec3218cecac61b6094076aaeefee
Gabriel-pagnan <gabrielviniciuspagnan@gmail.com>
2023-01-10 19:31:39 -0300

    update crud

--- src/shared/services/api/pessoas/PessoasService.ts
+++ src/shared/services/api/pessoas/PessoasService.ts
@@ -37,13 +37,54 @@ const getAll = async (page = 1, filter: ''):Promise<TPeapleFullCount | Error> =>
   }
 };
 
-const getById = async ():Promise<any> =>{};
+const getById = async (id: number):Promise<IDetailPeaple | Error> =>{
+  try {
+    const urlRelative = `/pessoas/${id}`;
+    const { data } = await Api.get(urlRelative);
+    
+    if(data) {
+      return data;
+    }
+    return new Error('Erro ao consultar o registro');
+  } catch (error) {
+    console.log(error);
+    return new Error((error as {message: string}).message || 'Erro ao consultar o registro');
+  }
+};
 
-const create = async ():Promise<any> =>{};
+const create = async (dados: Omit<IDetailPeaple, 'id'>):Promise<number | Error> =>{
+  try {
+    const { data } = await Api.post<IDetailPeaple>('/pessoas', dados);
+        
+    if(data) {
+      return data.id;
+    }
+    return new Error('Erro ao criar o registro');
+  } catch (error) {
+    console.log(error);
+    return new Error((error as {message: string}).message || 'Erro ao criar o registro');
+  }
+};
 
-const updateById = async ():Promise<any> =>{};
+const updateById = async (id: number, dados: IDetailPeaple):Promise<void | Error> =>{
+  try {
+    await Api.put(`/pessoas/${id}`, dados);
+    return new Error('Erro ao atualizar o registro');
+  } catch (error) {
+    console.log(error);
+    return new Error((error as {message: string}).message || 'Erro ao atualizar o registro');
+  }
+};
 
-const deleteById = async ():Promise<any> =>{};
+const deleteById = async (id: number):Promise<void | Error> =>{
+  try {
+    await Api.delete(`/pessoas/${id}`);
+    return new Error('Erro ao deletar o registro');
+  } catch (error) {
+    console.log(error);
+    return new Error((error as {message: string}).message || 'Erro ao deletar o registro');
+  }
+};
 
 
 export const PessoasService = {
