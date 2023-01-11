7158fa231e3734a579e0b61dccb8708818b4ab73
Gabriel-pagnan <gabrielviniciuspagnan@gmail.com>
2023-01-11 19:08:00 -0300

    configuração de pesquisa

--- src/pages/pessoas/ListagemPessoas.tsx
+++ src/pages/pessoas/ListagemPessoas.tsx
@@ -1,26 +1,33 @@
 import { useEffect, useMemo } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { FerramentaListagem } from '../../shared/components';
+import { useDebounce } from '../../shared/hooks';
 import { LayoutBase } from '../../shared/layouts';
 import { PessoasService } from '../../shared/services/api/pessoas/PessoasService';
 
 export const LitagemPessoas: React.FC = () =>{
   const [searchParams, setSearchParms] = useSearchParams();
+  const {debounce} = useDebounce(3000);
 
   const search = useMemo(()=>{
     return searchParams.get('search') || '';
   }, [searchParams]);
 
   useEffect(()=>{
-    PessoasService.getAll(1, search)
-      .then((result) => {
-        if(result instanceof Error) {
-          alert(result.message);
-          return;
-        }else{
-          console.log(result.data[0]);
-        }
-      });
+
+    debounce(() => {
+
+      PessoasService.getAll(1, search)
+        .then((result) => {
+          if(result instanceof Error) {
+            alert(result.message);
+            return;
+          }else{
+            console.log(result.data[0]);
+          }
+        });
+    });
+
   }, [search]);
 
   return(
