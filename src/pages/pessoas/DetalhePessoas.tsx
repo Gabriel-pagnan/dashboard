bc4b5c126a6decadcb52a043b670738ce847307a
Gabriel-pagnan <gabrielviniciuspagnan@gmail.com>
2023-01-13 19:48:30 -0300

    add delete register

--- src/pages/pessoas/DetalhePessoas.tsx
+++ src/pages/pessoas/DetalhePessoas.tsx
@@ -1,21 +1,58 @@
+import { CircularProgress } from '@mui/material';
+import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { FerramentaDetalhe } from '../../shared/components';
 import { LayoutBase } from '../../shared/layouts';
+import { PessoasService } from '../../shared/services/api/pessoas/PessoasService';
 
 export const DetalhePessoas: React.FC = () =>{
   const {id = 'novo'} = useParams<'id'>();
   const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const [name, setName] = useState('');
+
+
+  useEffect(() => {
+    if(id !== 'novo') {
+      setIsLoading(true);
+
+      PessoasService.getById(Number(id))
+        .then((result) => {
+          setIsLoading(false);
+
+          if(result instanceof Error) {
+            toast.error(result.message);
+            navigate('/pessoas');
+          }else{
+            setName(result.fullName);
+            
+          }
+        });
+    }
+  }, []);
 
   const handleSave = () =>{
     toast.success('Cadastro salvo.');
   };
-  const handleDelete = () =>{
-    toast.success('Cadastro deletado.');
+
+  const handleDelete = (id: number) => {
+    if(confirm('Deseja apagar este registro?')) {
+      PessoasService.deleteById(id)
+        .then(result => {
+          if(result instanceof Error){
+            toast.error(result.message);
+          }else{
+            toast.success('Registro deletado.');
+            navigate('/pessoas');
+          }
+        });
+    }
   };
+
   return(
     <LayoutBase 
-      title={(id !== 'novo') ? 'Editar cadastro' : 'Cadastrar' }
+      title={(id !== 'novo') ? `${name}` : 'Cadastrar' }
       barraFerramentas={
         <FerramentaDetalhe 
           showNewButton={id !== 'novo'}
@@ -26,11 +63,14 @@ export const DetalhePessoas: React.FC = () =>{
           
           handleClickBack={() => navigate(id !== 'novo' ? '/pessoas' : '/home')}
           handleClickNew={() => navigate('/pessoas/detalhe/novo')}
-          handleClickDelete={handleDelete}
+          handleClickDelete={() => handleDelete(Number(id))}
           handleClickSave={handleSave}
           handleClickSaveBack={handleSave}
         />
       }>
+      {isLoading &&(
+        <CircularProgress variant='indeterminate'/>
+      )}
       <p>dasda {id}</p>
     </LayoutBase>
   );
